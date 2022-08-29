736051f39e6be2c9c33be4e895ee1e8f24762460
Debriks <deborah.robbiano@gmail.com>
2022-08-29 12:34:23 +0200

    App Set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container d-flex justify-content-center">
+        <div className="Title">
+          <h1 class="text-center">Dictionary</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
