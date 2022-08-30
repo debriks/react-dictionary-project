8d52c11c21641c5543f036f59b92956f3b76714b
Debriks <deborah.robbiano@gmail.com>
2022-08-30 17:47:47 +0200

    Built the Results & Meaning components

---
+++ src/Meaning.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Meaning.css";
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h3>
+        {" "}
+        <em> {props.meaning.partOfSpeech} </em>{" "}
+      </h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <br />
+            <em>{definition.example}</em>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
+
+    // Documentation : https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -20,15 +25,24 @@ export default function SearchEngine() {
   }
 
   return (
-    <div className="SearchEngine d-flex flex-row">
-      <div className="SearchEngine__form">
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange}></input>
-        </form>
+    <div className="SearchEngine d-flex flex-column">
+      <div className="SearchEngine__section d-flex flex-row">
+        <div className="SearchEngine__form">
+          <form onSubmit={search}>
+            <input
+              type="search"
+              placeholder="Type a word"
+              onChange={handleKeywordChange}
+            ></input>
+          </form>
+        </div>
+        <div>
+          <button className="SearchEngine__btn" onClick={handleKeywordChange}>
+            <p> Search</p>
+          </button>
+        </div>
       </div>
-      <button className="SearchEngine__btn" onClick={handleKeywordChange}>
-        <p> Search</p>
-      </button>
+      <Results results={results} />
     </div>
   );
 }
